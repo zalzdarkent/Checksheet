c50146f926bf47d7a63fe52e161f9b566bb74603
Zalz <arszalzdarkent@gmail.com>
2025-03-14 14:36:40 +0700

    masih belum bisa relasi

--- app/Views/checksheet/tabel.php
+++ app/Views/checksheet/tabel.php
@@ -26,9 +26,6 @@ function OkNg($jumlahKolom)
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-4">
     <h2 class="text-center">Checksheet <?= esc($checksheet['mesin']) ?></h2>
-    <div class="d-flex justify-content-end mt-4 mb-4">
-        <a href="/checksheet/tambah"><button class="btn btn-primary">Tambah Item</button></a>
-    </div>
     <div class="card p-2 mt-3">
         <table class="table table-borderless" id="dataTable">
             <tbody>
